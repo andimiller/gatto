d6b2bd8fb10a64cb91ad6d6e388239ec0628cd52
Andi Miller <andi@andimiller.net>
2019-09-16 18:00:16 +0100

    add a few basic combinators

--- src/main/scala/net/andimiller/gatto/Parser.scala
+++ src/main/scala/net/andimiller/gatto/Parser.scala
@@ -1,7 +1,7 @@
 package net.andimiller.gatto
 
 import cats._
-import cats.data.{EitherT, StateT}
+import cats.data.{EitherT, NonEmptyList, StateT}
 import cats.implicits._
 
 import scala.language.higherKinds
@@ -9,15 +9,44 @@ import scala.language.higherKinds
 object Parser {
 
   case class ParserT[F[_]: Monad, S, O](value: StateT[EitherT[F, String, *], S, O]) {
+    // ways to run the parser
+    def parse(s: S): EitherT[F, String, (S, O)] = value.run(s)
+
+
+    // combinators
     def orElse[O2 <: O](other: ParserT[F, S, O2]): ParserT[F, S, O] = ParserT(value.orElse(other.value.widen[O]))
+
     def subFlatMap[O2](subParser: ParserT[F, O, O2]): ParserT[F, S, O2] =
       ParserT(value.flatMapF { o =>
         subParser.value.run(o).map(_._2)
       })
-    def parse(s: S): EitherT[F, String, (S, O)] = value.run(s)
+
+    def validate(f: O => Boolean, otherwise: String): ParserT[F, S, O] =
+      ParserT(value.flatMapF(o => EitherT.fromEither[F](Either.cond(f(o), o, otherwise))))
+
+    def filter(f: O => Boolean): ParserT[F, S, O] = validate(f, "Failed filter function")
+
+    def rep(implicit M: Monad[ParserT[F, S, *]]): ParserT[F, S, List[O]] =
+      List
+        .empty[Either[String, O]]
+        .iterateWhileM { os =>
+          this.attempt.map(o => o :: os)
+        }(_.forall(_.isRight))
+        .map(_.separate._2.reverse)
+
+    def rep1(implicit M: Monad[ParserT[F, S, *]]): ParserT[F, S, NonEmptyList[O]] =
+      (this, this.rep).tupled.map { case (o, os) => NonEmptyList(o, os) }
+
+    def opt: ParserT[F, S, Option[O]] =
+      this.map(_.some).orElse(ParserT.pure[F, S, Option[O]](none))
+
   }
+
   object ParserT {
-//    type InternalParserT[F[_], S, O] = StateT[EitherT[F, String, ?], S, O]
+    def pure[F[_]: Monad, S, O](o: O): ParserT[F, S, O] =
+      ParserT(StateT.liftF(EitherT.pure[F, String](o)))
+
+    //    type InternalParserT[F[_], S, O] = StateT[EitherT[F, String, ?], S, O]
     def liftF[F[_]: Monad, S, O](f: S => F[Either[String, (S, O)]]): ParserT[F, S, O] =
       ParserT(StateT({ s: S =>
         EitherT(f(s))
@@ -29,18 +58,23 @@ object Parser {
         val underlyingMonadError = MonadError[StateT[EitherT[F, String, *], S, *], String]
 
         def pure[A](x: A): ParserT[F, S, A] = ParserT(underlying.pure(x))
+
         def flatMap[A, B](fa: ParserT[F, S, A])(f: A => ParserT[F, S, B]): ParserT[F, S, B] =
           ParserT(fa.value.flatMap(r => f(r).value))
+
         def tailRecM[A, B](a: A)(f: A => ParserT[F, S, Either[A, B]]): ParserT[F, S, B] =
           ParserT(underlying.tailRecM(a)(f.map(_.value)))
+
         def raiseError[A](e: String): ParserT[F, S, A] =
           ParserT(underlyingMonadError.raiseError(e))
+
         def handleErrorWith[A](fa: ParserT[F, S, A])(f: String => ParserT[F, S, A]): ParserT[F, S, A] =
           ParserT(underlyingMonadError.handleErrorWith(fa.value)(f.map(_.value)))
       }
   }
 
   abstract class Dsl[F[_]: Monad: Defer, S, T: Eq: Show](implicit cpf: CanParseFrom[S, T]) {
+
     import ParserT._
 
     def takeOne: ParserT[F, S, T] = ParserT.liftF[F, S, T] { s: S =>
@@ -50,6 +84,13 @@ object Parser {
         )
       }
     }
+
+    def takeWhile(f: T => Boolean): ParserT[F, S, S] = takeOne.validate(f, "").rep.map(cpf.combine)
+
+    def takeWhile1(f: T => Boolean): ParserT[F, S, S] = takeOne.validate(f, "").rep1.map(ss => cpf.combine(ss.toList))
+
+    def takeIf(f: T => Boolean, otherwise: String): ParserT[F, S, T] = takeOne.validate(f, otherwise)
+
     def get: ParserT[F, S, T] = ParserT.liftF[F, S, T] { s: S =>
       Defer[F].defer {
         Monad[F].point(
@@ -57,6 +98,7 @@ object Parser {
         )
       }
     }
+
     def literal(target: T): ParserT[F, S, T] = ParserT.liftF[F, S, T] { s: S =>
       Defer[F].defer {
         Monad[F].point(
@@ -72,9 +114,10 @@ object Parser {
         )
       }
     }
+
     def literals(target: S): ParserT[F, S, S] = cpf.split(target).traverse(t => literal(t)).map(cpf.combine)
 
-    def takeIf[O](f: T => Either[String, O]): ParserT[F, S, O] = ParserT.liftF[F, S, O] { s: S =>
+    /*    def takeIf[O](f: T => Either[String, O]): ParserT[F, S, O] = ParserT.liftF[F, S, O] { s: S =>
       Defer[F].defer {
         Monad[F].point(
           Either
@@ -86,6 +129,9 @@ object Parser {
         )
       }
     }
+  }
+   */
+
   }
 
 }
--- src/test/scala/ParserTSpec.scala
+++ src/test/scala/ParserTSpec.scala
@@ -6,22 +6,68 @@ import net.andimiller.gatto.Parser
 import net.andimiller.gatto.Parser._
 
 class ParserTSpec extends WordSpec with MustMatchers {
-  val dsl = new Dsl[IO, String, Char] {}
+  val dsl = new Dsl[Eval, String, Char] {}
   import dsl._
 
   "basic parsers which use literals" should {
     "work with normal data" in {
-      val literalParser: Parser.ParserT[IO, String, (Char, Char)] = for {
+      val literalParser: Parser.ParserT[Eval, String, (Char, Char)] = for {
         a <- literal('a')
         b <- literal('b')
       } yield (a, b)
-      val literalsParser: Parser.ParserT[IO, String, (String, String)] = for {
+      val literalsParser: Parser.ParserT[Eval, String, (String, String)] = for {
         as <- literals("aa")
         bs <- literals("bb")
       } yield (as, bs)
-      literalParser.parse("ab").value.unsafeRunSync() must equal(Right("", ('a', 'b')))
-      literalParser.parse("abc").value.unsafeRunSync() must equal(Right("c", ('a', 'b')))
-      literalParser.parse("nah").value.unsafeRunSync() must equal(Left("Expected: a, Got: n"))
+      literalParser.parse("ab").value.value must equal(Right("", ('a', 'b')))
+      literalParser.parse("abc").value.value must equal(Right("c", ('a', 'b')))
+      literalParser.parse("nah").value.value must equal(Left("Expected: a, Got: n"))
+    }
+  }
+  "takeWhile" should {
+    "be able to takeWhile" in {
+      val takeAs = takeWhile(_ == 'a')
+      takeAs.parse("aaaaa").value.value must equal(Right("", "aaaaa"))
+      takeAs.parse("aaaaabc").value.value must equal(Right("bc", "aaaaa"))
+    }
+  }
+
+  "opt" should {
+    "make parsers optional" in {
+      val parser = for {
+        a <- literal('a')
+        b <- literal('b').opt
+        c <- literal('c')
+        d <- literal('d').opt
+        e <- literal('e')
+      } yield (a, b, c, d, e)
+      parser.parse("abcde").value.value must equal(Right("", ('a', Some('b'), 'c', Some('d'), 'e')))
+      parser.parse("ace").value.value must equal(Right("", ('a', None, 'c', None, 'e')))
+    }
+  }
+
+  "rep" should {
+    "make parsers repeat" in {
+      val parser: ParserT[Eval, String, (List[Char], List[Char])] = for {
+        letters <- takeIf(_.isLetter, "").rep
+        _       <- literal(' ')
+        digits  <- takeIf(_.isDigit, "").rep
+      } yield (letters, digits)
+      parser.parse("abc 123").value.value must equal(Right(("", (List('a', 'b', 'c'), List('1', '2', '3')))))
+      parser.parse(" ").value.value must equal(Right(("", (List.empty, List.empty))))
+    }
+  }
+
+  "subparsers" should {
+    "work" in {
+      val takeLowercase = takeWhile(_.isLower)
+      object Subparsers {
+        val dsl = new Dsl[Eval, List[Int], Int] {}
+        import dsl._
+        val evens    = takeIf(_ % 2 == 0, "must be even").as("even")
+        val odds     = takeIf(_ % 2 != 0, "must be odd").as("odd")
+        val combined = evens.orElse(odds)
+      }
     }
   }
 
